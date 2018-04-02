837461c27225179d0710568effb0d994181f9592
Kaneo <dingdongkane@gmail.com>
2018-04-02 07:33:50 +0100

    Revert "Open sign up modal when attempting to favorite as a guest"
    This reverts commit 4e5cf9fb141b5ace60684e7926fea8701a6bbcb8.

--- app/components/explore/favorite-button.js
+++ app/components/explore/favorite-button.js
@@ -29,7 +29,6 @@ export default Component.extend({
   }).drop(),
 
   createFavoriteTask: task(function* () {
-    if (!get(this, 'session.hasUser')) { return get(this, 'session').signUpModal(); }
     if (get(this, 'getFavoriteTask.isRunning')) { return; }
     const record = get(this, 'store').createRecord('category-favorite', {
       user: get(this, 'session.account'),
