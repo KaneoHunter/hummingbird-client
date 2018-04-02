ae7158ab4bd1dedf966f1db8d2e95bdf170019da
Kaneo <dingdongkane@gmail.com>
2018-04-02 07:33:49 +0100

    Revert "Request related categories on attribute update"
    This reverts commit 87743416e307c7da93819337053996d7676cf289.

--- app/components/explore/category-sidebar/related-categories.js
+++ app/components/explore/category-sidebar/related-categories.js
@@ -10,7 +10,7 @@ export default Component.extend({
   queryCache: service(),
   categories: concat('parent', 'siblings', 'children'),
 
-  didReceiveAttrs() {
+  init() {
     this._super(...arguments);
     get(this, 'getRelatedCategoriesTask').perform();
   },
