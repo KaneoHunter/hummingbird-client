401141fc61d4110ae77e34b20d5dac25973c55e1
Kaneo <dingdongkane@gmail.com>
2018-04-02 07:27:17 +0100

    Revert "Set up separate episode page template"
    This reverts commit fd57f7dee1bd3249a30ec3acb4f9b1af48a95b8c.

--- app/routes/anime/show/episodes/show.js
+++ app/routes/anime/show/episodes/show.js
@@ -1,5 +1,3 @@
 import UnitsRoute from 'client/routes/media/show/units/show';
 
-export default UnitsRoute.extend({
-  templateName: 'anime/show/episodes/show'
-});
+export default UnitsRoute.extend();
