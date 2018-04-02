6e1a71478272a4ff45d774e3570d716ce9abbf7f
Kaneo <dingdongkane@gmail.com>
2018-04-02 07:32:34 +0100

    Revert "Cleanup"
    This reverts commit dbcf8d4d68c5aa8ad3e36eb70cd1099798117239.

--- app/components/media/media-reaction-card.js
+++ app/components/media/media-reaction-card.js
@@ -3,13 +3,11 @@ import get from 'ember-metal/get';
 import set from 'ember-metal/set';
 import service from 'ember-service/inject';
 import computed, { or } from 'ember-computed';
-import { htmlSafe } from 'ember-string';
 import { task } from 'ember-concurrency';
 import errorMessages from 'client/utils/error-messages';
-import { image } from 'client/helpers/image';
 
 export default Component.extend({
-  classNames: ['col-sm-4', 'reaction-thumb', 'card'],
+  classNames: ['reaction-wrapper', 'col-sm-4'],
   isUpvoted: false,
   queryCache: service(),
   store: service(),
@@ -27,11 +25,6 @@ export default Component.extend({
       return true;
     }
   }),
-
-  posterImageStyle: computed('media.posterImage', function() {
-    const posterImage = image(get(this, 'media.posterImage'), 'medium');
-    return htmlSafe(`background-image: url("${posterImage}")`);
-  }).readOnly(),
 
   didReceiveAttrs() {
     this._super(...arguments);
